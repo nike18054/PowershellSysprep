fc5effd3a13a27d8543fb2b2fc3903bbe0df5a8c
Nicholas Glantz <nike18054@gmail.com>
2018-01-29 16:14:51 -0800

    Update RunPriorToSysprep.ps1

--- RunPriorToSysprep.ps1
+++ RunPriorToSysprep.ps1
@@ -1,23 +1,22 @@
 ﻿## Created by: Nicholas Glantz
 ## Function: Deletes a user account
-##           If the directory C:\ProgramData\Alloy Software exist it deletes it.
 ##           Cleans out the C:\Windows\Temp directory
-##           Starts sysprep.exe
+##           Starts an automated Sysprep batch script
 ## Date: November 30th, 2017
 
 ##Deletes USERNAME account
+##Enter the name of the user in place of USERNAME
+Get-WmiObject -Class Win32_UserProfile | where {$_.LocalPath.split()[-1] -eq 'Administrator.USERNAME'} | foreach {$_.Delete()}
 $Computername = $env:COMPUTERNAME
     $ADSIComp = [adsi]"WinNT://$Computername"
 $ADSIComp.Delete('User','USERNAME')
 
-##If the directory C:\ProgramData\Alloy Software exist it deletes it.
-$AlloySoftware = "C:\ProgramData\Alloy Software"
-if ( Test-Path -Path  $AlloySoftware){
-       rmdir $AlloySoftware
-}
-
 ##Cleans out the C:\Windows\Temp directory
 rmdir "C:\Windows\Temp\*" -ErrorAction SilentlyContinue
 
-##Starts sysprep.exe
-Start-Process C:\Windows\System32\Sysprep\sysprep.exe
+##Starts automatedsysprep batch script
+Start-Process C:\Windows\System32\Sysprep\automatedsysprep.bat
+
+##automatedsysprep.bat
+##cd /d C:\Windows\System32\Sysprep
+##sysprep.exe /oobe /shutdown /quiet
